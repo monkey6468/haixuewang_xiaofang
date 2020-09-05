7261d3064dcc18f9c75a8e4d16b24b448e5ab773
xiaoweihua <xiaoweihua@cnhnkj.com>
2020-09-05 10:13:29 +0800

    上传脚本爬取数据

---
+++ rename_file.py
@@ -0,0 +1,91 @@
+#-*-coding:utf-8-*-
+import os
+import json
+
+# 获取指定文件夹下所有文件名
+def get_file_names(file_dir):
+    for root, dirs, files in os.walk(file_dir):
+        #print(root) #当前目录路径
+        #print(dirs) #当前路径下所有子目录
+        #print(files) #当前路径下所有非目录子文件
+        return files
+
+# 读全部内容
+def get_lesson_name_list(file_path):
+    txt_content_string = ''
+    with open(file_path, "r") as f:  # 打开文件
+        txt_content_string = f.read()  # 读取文件
+    
+    #print(txt_content_string)
+    lesson_list = txt_content_string.split(';')
+
+    # index:{}-name:{}-audioUrl:{}-lectureUrl:{}-videoUrl:{}-totalTime:
+    lesson_list_dict = []
+    for i in range(len(lesson_list)):
+        item = lesson_list[i]
+        # JSON字符串转换字典
+        item_dict = json.loads(item)
+        
+        lesson_dict = {}
+        lesson_dict['name'] = item_dict['name']
+        
+        videoUrl = item_dict['videoUrl']
+        # 只分割最后一个
+        videoUrl = videoUrl.rsplit("/",1)[1]
+        
+        lesson_dict['videoUrl'] = videoUrl
+        lesson_list_dict.append(lesson_dict)
+    return lesson_list_dict
+    
+def rename_video_file(file_dir_path, save_file_dir_path, files_list, lesson_list_dict):
+    for i in range(len(files_list)):
+        item_i = files_list[i]
+        old_Path = file_dir_path+'/'+item_i
+        #print(old_Path)
+        
+        for j in range(len(lesson_list_dict)):
+            item_j = lesson_list_dict[j]
+            item_j_name = item_j['name']
+            item_j_videoUrl = item_j['videoUrl']
+            if (item_i == item_j_videoUrl):
+                new_Path = save_file_dir_path+'/'+'{}、'.format(i+1)+item_j_name+'.mp4'
+                #print(new_Path)
+                os.rename(old_Path, new_Path)
+
+    print('重命名 成功')
+
+if __name__ == '__main__':
+    input_type = input('首先请保重csrf_token、cookie正确(根据调试具有这两个参数实时性)，请输入goodsModuleId：\n1-实务教材精讲\n2-综合教材精讲\n3-案例教材精讲\n：')
+    
+    video_file_name = 'videos'
+    dirt_path = os.getcwd()
+    file_dir_path = dirt_path+'/'+video_file_name
+    print(file_dir_path)
+    
+    # 获取指定文件夹下所有文件名
+    files_list = get_file_names(file_dir_path)
+    #print(files_list)
+    
+    txt_file_name = "1、实务教材精讲"
+    # 必须 *
+    if int(input_type) == 1:
+        txt_file_name = '1、实务教材精讲'
+    elif int(input_type) == 2:
+        txt_file_name = '2、综合教材精讲'
+    else:
+        filename = '3、案例教材精讲'
+    file_path = dirt_path+'/'+'脚本数据'+'/'+txt_file_name+'.txt'
+    #print(file_path)
+    
+    # 获取课程信息列表（字典数组）
+    lesson_list_dict = get_lesson_name_list(file_path)
+    #print(lesson_list_dict)
+    
+    save_file_dir_path = dirt_path+'/'+txt_file_name
+    # 判断目标是否存在
+    if (os.path.exists(save_file_dir_path) == False):
+        # 创建目录
+        os.mkdir(save_file_dir_path)
+    
+    rename_video_file(file_dir_path, save_file_dir_path, files_list, lesson_list_dict)
+    
---
+++ 脚本数据/get_all_lesson_list_by_type.py
@@ -0,0 +1,159 @@
+# -*- coding:UTF-8 -*-
+import requests
+import json
+import csv
+import os
+
+
+def write_to_csv(filename, lesson_list):
+    with open(filename,"w") as csvfile:
+        writer = csv.writer(csvfile)
+
+        # 判断是否有内容
+        size = os.path.getsize(filename)
+        if size == 0:
+            writer.writerow([u'序号', u'名称', u'音频地址', u'课件地址', u'视频地址', u'视频时长'])
+
+        #写入多行用writerows
+        for item in lesson_list:
+            index = item['index']
+            name = item['name']
+            audioUrl = item['audioUrl']
+            lectureUrl = item['lectureUrl']
+            videoUrl = item['videoUrl']
+            totalTime = item['totalTime']
+            
+            writer.writerow([index, name, audioUrl, lectureUrl, videoUrl, totalTime])
+        
+
+def get_headers(cookie):
+    headers = {
+       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+       'Accept': '*/*',
+       'Accept-Encoding': 'gzip, deflate, br',
+       'Accept-Language': 'zh-CN,zh;q=0.9',
+       'Content-Type':'application/form-urlencode',
+       'Cookie':cookie
+    }
+    return headers
+
+# 获取所有课程列表
+def get_lesson_list(csrf_token, goodsModuleId , url, headers):
+    url='{}?csrf_token={}&goodsModuleId={}'.format(url,csrf_token,goodsModuleId)
+    #print(url)
+    
+    lesson_list_string = []
+    lesson_list_dict = []
+    try:
+        response = requests.get(url, data=None, headers=headers)
+        if not response.status_code == 200:
+            print('请求失败')
+            return lesson_list_string,lesson_list_dict
+            
+        result_dict = json.loads(response.text)['data']
+        
+        result_errorCode = json.loads(response.text)['code']
+        if result_errorCode != 0:
+            print("======列表获取成功======")
+            for i in range(len(result_dict['videoList'])):
+                item = result_dict['videoList'][i]
+                # 获取单个课程信息
+                url_lesson_list = 'http://highso.cn/study/pc/video/download'
+                videoId = item['id']
+                totalTime = item['totalTime']
+                m, s = divmod(int(totalTime), 60)
+                totalTime = "%02d:%02d" % (m, s)
+                lesson_dict = get_lesson_info(csrf_token, videoId, url_lesson_list, headers)
+                
+                i +=1
+                print("      正在获取数据{}/{}      ".format(i, len(result_dict['videoList'])))
+                
+                list_dict = {}
+                list_dict['index'] = i
+                list_dict['name'] = item['name']
+                list_dict['audioUrl'] = lesson_dict['audioUrl']
+                list_dict['lectureUrl'] = lesson_dict['lectureUrl']
+                list_dict['videoUrl'] = lesson_dict['videoUrl']
+                list_dict['totalTime'] = totalTime
+                print(list_dict)
+                lesson_list_dict.append(list_dict)
+                
+                row_data = json.dumps(list_dict)
+                #print(row_data)
+                lesson_list_string.append(row_data)
+        else:
+            print("======列表获取失败======")
+        return lesson_list_string,lesson_list_dict
+    except Exception as e:
+            print("======获取失败======{}".format(e))
+            return lesson_list_string,lesson_list_dict
+
+# 获取单个课程信息
+def get_lesson_info(csrf_token, videoId, url, headers):
+    url='{}?csrf_token={}&videoId={}'.format(url,csrf_token,videoId)
+    #print(url)
+    response = requests.get(url, data=None, headers=headers)
+    result_dict = json.loads(response.text)['data']
+    
+    lesson_dict = {}
+    lesson_dict['audioUrl'] = result_dict['audioUrl']
+    lesson_dict['lectureUrl'] = result_dict['lectureUrl']
+    videoUrl = result_dict['videoUrl'].split('?')[0]
+    lesson_dict['videoUrl'] = videoUrl
+    #print(lesson_dict)
+    return lesson_dict
+
+
+def get_save_all_lesson(file_name, csrf_token, goodsModuleId, url_lesson_list, headers):
+    url_lesson_list_string, lesson_list_dict = get_lesson_list(csrf_token, goodsModuleId, url_lesson_list, headers)
+    #print(lesson_list)
+    
+    print("======正在写入文件======")
+    write_to_csv('{}.csv'.format(file_name), lesson_list_dict)
+    print("======写入文件完成======")
+    
+    lesson_list_all_string = ''
+    for i in range(len(url_lesson_list_string)):
+        item = url_lesson_list_string[i]
+        if (i != len(url_lesson_list_string)-1):
+            lesson_list_all_string += item+';'
+        else:
+            lesson_list_all_string += item
+    #print(lesson_list_all_string)
+        
+    # 将结果输出并存储
+    with open('{}.txt'.format(file_name),"w") as file:
+        file.write(lesson_list_all_string+'\n')
+                
+    # http://highso.cn/study/pc/video/download?csrf_token=be2R5k&videoId=98181
+    # http://highso.cn/study/pc/goodsmodule/findbyid?csrf_token=be2R5k&goodsModuleId=18385
+    
+if __name__ == '__main__':
+    
+    input_type = input('首先请保重csrf_token、cookie正确(根据调试具有这两个参数实时性)，请输入goodsModuleId：\n1-实务教材精讲:18385\n2-综合教材精讲:18387\n3-案例教材精讲:18389\n：')
+    # 必须 *
+    csrf_token = 'KIMCIM'
+    
+    # 必须 *
+    cookie='__jsluid_h=6bf1eb16beb6abe31ebd64c264e63bb8; gr_user_id=cbf469e0-8b64-4865-bab4-7c909501708f; grwng_uid=c522ecc4-e18a-4b91-b7d1-1245d3c547e2; deviceType=NORMAL; pageNum=0; 88da63a710494189_gr_last_sent_cs1=28770711; haixue_playback_bj=1599235513178; H_U_C=28770711%2C18520156468_6598%40haixue.com%2C%E5%AD%A6%E5%91%9828770711; U_N_C=18520156468; pass_sec=8df95c2dcd4b492a8885890f4114a1a3.28770711.1599265124674.web.bd24c901af498381232d01107699a102; csrf_token=KIMCIM; security_refresh=8df95c2dcd4b492a8885890f4114a1a3.28770711.1599265124674.web.bd24c901af498381232d01107699a102; 88da63a710494189_gr_session_id=8ff7d65c-ae25-4429-afea-181a5e2e03b2; 88da63a710494189_gr_last_sent_sid_with_cs1=8ff7d65c-ae25-4429-afea-181a5e2e03b2; security_access=eyJhbGciOiJSUzUxMiJ9.eyJ1aWQiOjI4NzcwNzExLCJzdWIiOiJ3ZWIiLCJpc3MiOiJwYXNzcG9ydCIsImV4cCI6MTU5OTI3MDg0OCwiaWF0IjoxNTk5MjcwNTQ4LCJqdGkiOiJjYzQ5NzM2Ny0wNjE0LTQ4MmYtODhlZi0wOGY4Yzg3NGQyZTgifQ.O1E7qQmo-vUpB4yqDEnoIldHjCoui0uP4bYqqKmRI-_FA2Wc-S3SWxahQGW3IhpRTkQkFzymYGYnIMjSyLmANmR_1rqou5oMb_UmJLOfOW1rQdH5aAJjmaRisRps03UJjo3981pt5XvZo4k6VR15TGXdzZNLR-l99WWraFZ71cg; 88da63a710494189_gr_session_id_8ff7d65c-ae25-4429-afea-181a5e2e03b2=true; security=55bf8dbb-70c0-4ae0-be83-5f24ce698510.1599270548352.28770711.web.194fa5df85b99ec12c73f52f0d3f7286; 88da63a710494189_gr_cs1=28770711; JSESSIONID=F2505932B66F0C969E5C2FBC131C37AE'
+    headers = get_headers(cookie)
+    
+    filename = ''
+    # 必须 *
+    goodsModuleId = ''
+    if int(input_type) == 1:
+        filename = '1、实务教材精讲'
+        goodsModuleId = 18385
+    elif int(input_type) == 2:
+        filename = '2、综合教材精讲'
+        goodsModuleId = 18387
+    else:
+        filename = '3、案例教材精讲'
+        goodsModuleId = 18389
+    
+    #print(filename)
+    
+    # 获取并保存所有课程列表
+    url_lesson_list = 'http://highso.cn/study/pc/goodsmodule/findbyid'
+    get_save_all_lesson(filename, csrf_token, goodsModuleId, url_lesson_list, headers)
+    print("======脚本执行完成======")
